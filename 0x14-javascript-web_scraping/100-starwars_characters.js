facedbf2efbf36858777710f67c3563c3cc7dbfc
patroba <patoomollo@gmail.com>
2023-04-25 22:35:30 +0000

    characters print out

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-async function getCharacterName(url) {
-  try {
-    const response = await request(url);
-    const body = JSON.parse(response.body);
-    return body.name;
-  } catch (error) {
-    console.error(`Error getting character name from ${url}: ${error}`);
-    return null;
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-}
-
-async function main() {
-  try {
-    const filmId = process.argv[2];
-    const filmUrl = `http://swapi.co/api/films/${filmId}`;
-    const response = await request(filmUrl);
-    const film = JSON.parse(response.body);
-    const characterUrls = film.characters;
-    const characterNames = await Promise.all(
-      characterUrls.map(url => getCharacterName(url))
-    );
-    console.log(characterNames.filter(name => name !== null));
-  } catch (error) {
-    console.error(`Error getting characters from film ID ${process.argv[2]}: ${error}`);
-  }
-}
-
-main();
-
+});
